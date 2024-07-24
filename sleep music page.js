90ff1d70f9ab350291f7102e7e96c5254a1b8761
nayanika12345 <kollinayanika2112@gmail.com>
2024-07-24 22:59:55 +0530

    Add files via upload

---
+++ sleep music page.js
@@ -0,0 +1,22 @@
+< div class = "utube2" >
+    <
+    div align = "center"
+class = "embed-responsive embed-responsive-16by9 m2utube" >
+    <
+    iframe class = "embed-responsive-item"
+src = "https://www.youtube.com/embed/8Vq-RkdwArA?rel=0" > < /iframe> <
+    /div> <
+    p class = "m2para2" > There are some really essential health tips you should know
+if you witness or a caught in an avalanche which could save lives The videos in this program were created in partnership with La Croix Rouge and Croissant Rouge(IFRC) < /p> <
+    /div> <
+    div class = "utube2" >
+    <
+    div align = "center"
+class = "embed-responsive embed-responsive-16by9 m2utube" >
+    <
+    iframe class = "embed-responsive-item"
+src = "https://www.youtube.com/embed/X90kUtxMd_Q?rel=0" > < /iframe> <
+    /div> <
+    p class = "m2para2" > In the
+case ofan avalanche, it’ s critical to know how to use your rescue gear efficiently.Here, we present the basics to a proper beacon search, as well as the most efficient probing and shoveling techniques with ski patroller and avalanche educator Harry Hamlin.Before you head out, take a professional avalanche course to gain the in -field skills and knowledge you need. < /p> <
+    /div>
